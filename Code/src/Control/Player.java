1b45e16b3123f281cf12882af2674b25d5af0052
ChrisSayyeau <chris0608@sympatico.ca>
2015-02-04 13:11:30 -0500

    Started starting the game

--- Code/src/Control/Player.java
+++ Code/src/Control/Player.java
@@ -1,5 +1,15 @@
+
 package Control;
+
+import CharacterProfiles.Dwarf;
 
 public class Player {
 
+	//constructor
+	Player(){
+		
+		//for simplisity we will set the first character to 
+		Dwarf profile = new Dwarf();
+		
+	}
 }
--- Code/src/Control/Server.java
+++ Code/src/Control/Server.java
@@ -2,4 +2,16 @@ package Control;
 
 public class Server {
 
+	
+	void main(){
+		//We 'll eventually be networking so the client's will have to connect with server which will call the game
+		//FOr now the server will create the players
+		
+		Player player1 = new Player();
+		
+		Game game = new Game();
+		
+		game.startGame(player1);
+	}
+	
 }
