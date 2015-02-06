9886f54a523a4d7d08ee8cfa3d12f316c105c036
christophersayyeau <chris0608@sympatico.ca>
2015-02-06 09:56:14 -0500

    Started the map

--- Code/src/Model/Map.java
+++ Code/src/Model/Map.java
@@ -2,5 +2,15 @@ package Model;
 
 public class Map {
 
+	//an array of tiles, the values will be hardcoded in
+	MapTiles [] mapTiles = new MapTiles[20];
 
+	public void build() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	//NOTE: if there is time create a way of building the board yourself
 }
